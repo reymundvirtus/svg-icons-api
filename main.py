d01a3bdfd0bf7a26459ea2537675682611d87666
reymundvirtus <reymundvirtus@gmail.com>
2025-06-30 04:10:36 +0800

    add html to root url

--- main.py
+++ main.py
@@ -1,44 +1,17 @@
 from fastapi import FastAPI
-from fastapi.responses import FileResponse
+from fastapi.responses import HTMLResponse
 from response import Response
-import os
 
 app = FastAPI()
 res = Response()
 
-NEUTRAL_ICON_FOLDER = "assets/icons/neutral"
-LIGHT_ICON_FOLDER = "assets/icons/light"
-DARK_ICON_FOLDER = "assets/icons/dark"
-
-WORDMARK_ICON_FOLDER = "assets/wordmark"
-
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 def read_root():
-    return { "Hello": "World!" }
+    return res.return_index_html()
 
-@app.get("/icons/neutral/{icon_name}")
-def get_neutral_icon(icon_name: str):
-    icon_path = os.path.join(NEUTRAL_ICON_FOLDER, f"{icon_name}.svg")
-
-    if os.path.isfile(icon_path):
-        return FileResponse(icon_path, media_type="image/svg+xml")
-
-    raise res.return_not_found()
-
-@app.get("/icons/light-variant/{icon_name}")
-def get_light_icon(icon_name: str):
-    icon_path = os.path.join(LIGHT_ICON_FOLDER, f"{icon_name}.svg")
-
-    if os.path.isfile(icon_path):
-        return FileResponse(icon_path, media_type="image/svg+xml")
-
-    raise res.return_not_found()
-
-@app.get("/icons/dark-variant/{icon_name}")
-def get_dark_icon(icon_name: str):
-    icon_path = os.path.join(DARK_ICON_FOLDER, f"{icon_name}.svg")
-
-    if os.path.isfile(icon_path):
-        return FileResponse(icon_path, media_type="image/svg+xml")
-
-    raise res.return_not_found()
+@app.get("/icons/{icon_type}/{icon_name}")
+def get_icon(icon_type: str, icon_name: str):
+    try:
+        return res.return_icon(icon_type=icon_type, icon_name=icon_name)
+    except:
+        raise res.return_not_found()
--- response.py
+++ response.py
@@ -1,6 +1,35 @@
 from fastapi import HTTPException
+from fastapi.responses import FileResponse
+import os
 
 class Response:
+    def __init__(self):
+        self.NEUTRAL_ICON_FOLDER = "assets/icons/neutral"
+        self.LIGHT_ICON_FOLDER = "assets/icons/light"
+        self.DARK_ICON_FOLDER = "assets/icons/dark"
+        self.WORDMARK_ICON_FOLDER = "assets/wordmark"
+
+    def return_index_html(self):
+        return FileResponse("index.html", media_type="text/html")
+
+    def return_icon(self, icon_type, icon_name):
+        if icon_type == "neutral":
+            icon_path = os.path.join(self.NEUTRAL_ICON_FOLDER, f"{icon_name}.svg")
+            return self.check_if_isfile(icon_path)
+        elif icon_type == "light":
+            icon_path = os.path.join(self.LIGHT_ICON_FOLDER, f"{icon_name}.svg")
+            return self.check_if_isfile(icon_path)
+        elif icon_type == "dark":
+            icon_path = os.path.join(self.DARK_ICON_FOLDER, f"{icon_name}.svg")
+            return self.check_if_isfile(icon_path)
+
+        raise self.return_not_found()
+    
+    def check_if_isfile(self, icon_path):
+        if os.path.isfile(icon_path):
+            return FileResponse(icon_path, media_type="image/svg+xml")
+
+        raise self.return_not_found()
 
     def return_not_found(self):
         return HTTPException(status_code=404, detail="Icon not found")
